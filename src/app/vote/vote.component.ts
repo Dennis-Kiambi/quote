8fc158efb61f02013b1b433f9d1a4ab3b466964b
Dennis-Kiambi <denniskiriinya@gmail.com>
2020-09-28 23:37:22 +0300

    add logic to add likes and dislikes

--- src/app/vote/vote.component.ts
+++ src/app/vote/vote.component.ts
@@ -1,5 +1,5 @@
 import { Component, OnInit, Output, EventEmitter, Input } from '@angular/core';
-import { Quote } from '../quote';
+import { Quote } from '../models';
 
 @Component({
   selector: 'app-vote',
